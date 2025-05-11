ee939162a4546467296e0a908496de89cac2c16b
Brent Cleary <brent.cleary@gmail.com>
2025-05-11 15:58:38 -0700

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Vector2 movement;
+    Rigidbody rigidBody;
+    [SerializeField] float moveSpeed = 5f;
+
+    public void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() 
+    {
+        HandleMovement();
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+        Debug.Log(movement);
+    }
+
+    public void HandleMovement()
+    {
+        Vector3 currentPosition = rigidBody.position;
+        Vector3 moveDirection = new Vector3(movement.x, 0f, movement.y);
+        Vector3 newPosition = currentPosition + moveDirection * (moveSpeed * Time.fixedDeltaTime);
+
+        rigidBody.MovePosition(newPosition);
+    }
+
+
+}
